58c7e53d4afdd5ffd5b8a3157cc6b81a0d31902b
nada <nada>
2021-12-13 13:00:41 +0100

    First commit

---
+++ Debugger Factorial/src/Package/Factorial.java
@@ -0,0 +1,18 @@
+package Package;
+
+
+import java.util.Scanner;
+
+public class Factorial {
+	public static void main(String[] args) {
+		Scanner r = new Scanner(System.in);
+		int num = r.nextInt();
+		long factorial = 1;
+		for (int i = 1; i <= num; i++) {
+			factorial = factorial * i;
+		}System.out.println(factorial);
+	}
+	
+	
+	
+}
